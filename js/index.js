6e5fa76550ac7912c7aa12cc1c531d2b381b6d1d
ssong-eu <sangyoon.song@gmail.com>
2018-11-01 09:48:05 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+let elementMaker = element => document.createElement(element);
+el = elementMaker("div");
+el.innerHTML = "WUT";
+document.body.appendChild(el);
